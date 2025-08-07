d18e66ed15d212213b0df688daeb3e04ba337f84
Your Name <youremail@yourdomain.com>
2025-08-07 12:36:06 +0200

    WSA: on / off cmd klikken geven nu een pulse aan de relais met lengte 100ms. Nu kan direct binden zonder coordinator +  minimum pulse lengte is 100msec vast zodat de lamp altijd de pulse ziet. Om brightness te veranderen; dit moet wel via coordinator; call toggle cmd  (deze zet de relay altijd AAN) en nadien off cmd (zal nog eens aanzetten en dan terug uit)

--- src/base_components/relay.c
+++ src/base_components/relay.c
@@ -8,8 +8,10 @@ void relay_init(relay_t *relay)
   relay_off(relay);
 }
 
+//WSA: zet aan en direct terug uit zodat de lamp van staat veranderd
 void relay_on(relay_t *relay)
 {
+  //Zet aan
   printf("relay_on\r\n");
   drv_gpio_write(relay->pin, relay->on_high);
   if (relay->off_pin)
@@ -21,10 +23,43 @@ void relay_on(relay_t *relay)
   {
     relay->on_change(relay->callback_param, 1);
   }
+  // wacht 75ms
+  WaitMs(100);
+
+  //Zet uit
+  printf("relay_off\r\n");
+  drv_gpio_write(relay->pin, !relay->on_high);
+  if (relay->off_pin)
+  {
+    drv_gpio_write(relay->off_pin, relay->on_high);
+  }
+  relay->on = 0;
+  if (relay->on_change != NULL)
+  {
+    relay->on_change(relay->callback_param, 0);
+  }
+
 }
 
+//WSA: zet aan en direct terug uit zodat de lamp van staat veranderd
 void relay_off(relay_t *relay)
 {
+  //Zet aan
+  printf("relay_on\r\n");
+  drv_gpio_write(relay->pin, relay->on_high);
+  if (relay->off_pin)
+  {
+    drv_gpio_write(relay->off_pin, !relay->on_high);
+  }
+  relay->on = 1;
+  if (relay->on_change != NULL)
+  {
+    relay->on_change(relay->callback_param, 1);
+  }
+  // wacht 75ms
+  WaitMs(100);
+  
+  //Zet uit
   printf("relay_off\r\n");
   drv_gpio_write(relay->pin, !relay->on_high);
   if (relay->off_pin)
@@ -38,15 +73,19 @@ void relay_off(relay_t *relay)
   }
 }
 
+//WSA: toggle relay zet de relay gewoon aan. Ik misbruik deze cluster zodat ik de brightness kan veranderen.
+//     nadien gewoon de relay_off nog eens callen als brightness_stop. dan gaat de relay nog eens aan (was al aan) en terug uit.
 void relay_toggle(relay_t *relay)
 {
-  printf("relay_toggle\r\n");
-  if (relay->on)
+  printf("relay_on\r\n");
+  drv_gpio_write(relay->pin, relay->on_high);
+  if (relay->off_pin)
   {
-    relay_off(relay);
+    drv_gpio_write(relay->off_pin, !relay->on_high);
   }
-  else
+  relay->on = 1;
+  if (relay->on_change != NULL)
   {
-    relay_on(relay);
+    relay->on_change(relay->callback_param, 1);
   }
 }
