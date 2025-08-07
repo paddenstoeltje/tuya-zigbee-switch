f78b42b98472e1257f0a172b63285ff83aad4e90
Your Name <youremail@yourdomain.com>
2025-08-07 16:08:54 +0200

    Only wait 75ms, remove toggles

--- src/base_components/relay.c
+++ src/base_components/relay.c
@@ -8,7 +8,6 @@ void relay_init(relay_t *relay)
   relay_off(relay);
 }
 
-//WSA: zet aan en direct terug uit zodat de lamp van staat veranderd
 void relay_on(relay_t *relay)
 {
   //Zet aan
@@ -26,42 +25,10 @@ void relay_on(relay_t *relay)
   // wacht 75ms
   WaitMs(75);
 
-  //Zet uit
-  printf("relay_off\r\n");
-  drv_gpio_write(relay->pin, !relay->on_high);
-  if (relay->off_pin)
-  {
-    drv_gpio_write(relay->off_pin, relay->on_high);
-  }
-  relay->on = 0;
-  if (relay->on_change != NULL)
-  {
-    relay->on_change(relay->callback_param, 0);
-  }
-  // wacht 75ms
-  WaitMs(75);
-
 }
 
-//WSA: zet aan en direct terug uit zodat de lamp van staat veranderd
 void relay_off(relay_t *relay)
 {
-  //Zet aan
-  printf("relay_on\r\n");
-  drv_gpio_write(relay->pin, relay->on_high);
-  if (relay->off_pin)
-  {
-    drv_gpio_write(relay->off_pin, !relay->on_high);
-  }
-  relay->on = 1;
-  if (relay->on_change != NULL)
-  {
-    relay->on_change(relay->callback_param, 1);
-  }
-  // wacht 75ms
-  WaitMs(75);
-  
-  //Zet uit
   printf("relay_off\r\n");
   drv_gpio_write(relay->pin, !relay->on_high);
   if (relay->off_pin)
@@ -73,23 +40,17 @@ void relay_off(relay_t *relay)
   {
     relay->on_change(relay->callback_param, 0);
   }
-  // wacht 75ms
-  WaitMs(75);
 }
 
-//WSA: toggle relay zet de relay gewoon aan. Ik misbruik deze cluster zodat ik de brightness kan veranderen.
-//     nadien gewoon de relay_off nog eens callen als brightness_stop. dan gaat de relay nog eens aan (was al aan) en terug uit.
 void relay_toggle(relay_t *relay)
 {
-  printf("relay_on\r\n");
-  drv_gpio_write(relay->pin, relay->on_high);
-  if (relay->off_pin)
+  printf("relay_toggle\r\n");
+  if (relay->on)
   {
-    drv_gpio_write(relay->off_pin, !relay->on_high);
+    relay_off(relay);
   }
-  relay->on = 1;
-  if (relay->on_change != NULL)
+  else
   {
-    relay->on_change(relay->callback_param, 1);
+    relay_on(relay);
   }
 }
